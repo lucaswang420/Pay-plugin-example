666a2843bc92e371d95ccfcc7b92808216bd9aa0
vilas <vilas900420@gmail.com>
2026-04-09 18:06:47 +0800

    feat: add RefundService skeleton
    Create RefundService with idempotency support:
    - Interface matches PayPlugin refund methods
    - Idempotency checking via IdempotencyService
    - Placeholder implementations for main methods
    
    Next: Extract full business logic from PayPlugin.cc

---
+++ PayBackend/services/RefundService.cc
@@ -0,0 +1,95 @@
+#include "RefundService.h"
+#include "../models/PayRefund.h"
+#include "../utils/PayUtils.h"
+#include <drogon/drogon.h>
+#include <random>
+#include <sstream>
+#include <iomanip>
+
+using namespace drogon;
+
+RefundService::RefundService(
+    std::shared_ptr<WechatPayClient> wechatClient,
+    std::shared_ptr<DbClient> dbClient,
+    std::shared_ptr<IdempotencyService> idempotencyService)
+    : wechatClient_(wechatClient), dbClient_(dbClient),
+      idempotencyService_(idempotencyService) {
+}
+
+void RefundService::createRefund(
+    const CreateRefundRequest& request,
+    const std::string& idempotencyKey,
+    RefundCallback&& callback) {
+
+    // Calculate request hash for idempotency
+    std::string requestStr = Json::writeString(Json::StreamWriterBuilder(),
+        [&request]() {
+            Json::Value req;
+            req["order_no"] = request.orderNo;
+            req["amount"] = request.amount;
+            req["reason"] = request.reason;
+            return req;
+        }());
+
+    // Simple hash (in production, use proper cryptographic hash)
+    std::string requestHash = std::to_string(std::hash<std::string>{}(requestStr));
+
+    // Check idempotency
+    idempotencyService_->checkAndSet(
+        idempotencyKey,
+        requestHash,
+        [&request]() {
+            Json::Value req;
+            req["order_no"] = request.orderNo;
+            req["amount"] = request.amount;
+            return req;
+        }(),
+        [this, request, callback](bool canProceed, const Json::Value& cachedResult) {
+            if (!canProceed) {
+                // Idempotency conflict
+                Json::Value error;
+                error["code"] = 1004;
+                error["message"] = "Idempotency conflict: different parameters for same key";
+                callback(error, std::make_error_code(std::errc::operation_in_progress));
+                return;
+            }
+
+            if (!cachedResult.isNull()) {
+                // Return cached result
+                callback(cachedResult, std::error_code());
+                return;
+            }
+
+            // TODO: Extract business logic from PayPlugin.cc
+            // This is a placeholder - full implementation will be in subsequent tasks
+            Json::Value response;
+            response["code"] = 0;
+            response["message"] = "Refund created (placeholder)";
+            Json::Value data;
+            data["refund_no"] = request.refundNo;
+            data["order_no"] = request.orderNo;
+            response["data"] = data;
+
+            callback(response, std::error_code());
+        }
+    );
+}
+
+void RefundService::queryRefund(
+    const std::string& refundNo,
+    RefundCallback&& callback) {
+
+    // TODO: Implement in subsequent task
+    Json::Value response;
+    response["code"] = 0;
+    response["message"] = "Query refund (placeholder)";
+    callback(response, std::error_code());
+}
+
+void RefundService::syncRefundStatusFromWechat(
+    const std::string& refundNo,
+    std::function<void(const std::string& status)>&& callback) {
+
+    // TODO: Implement in subsequent task
+    callback("unknown");
+}
---
+++ PayBackend/services/RefundService.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <drogon/orm/DbClient.h>
+#include <drogon/nosql/RedisClient.h>
+#include "IdempotencyService.h"
+#include "WechatPayClient.h"
+#include <json/json.h>
+#include <functional>
+#include <memory>
+#include <string>
+
+struct CreateRefundRequest {
+    std::string orderNo;
+    std::string refundNo;
+    std::string amount;
+    std::string reason;
+    std::string notifyUrl;
+};
+
+class RefundService {
+public:
+    using RefundCallback = std::function<void(const Json::Value& result, const std::error_code& error)>;
+
+    RefundService(
+        std::shared_ptr<WechatPayClient> wechatClient,
+        std::shared_ptr<drogon::orm::DbClient> dbClient,
+        std::shared_ptr<IdempotencyService> idempotencyService
+    );
+
+    void createRefund(
+        const CreateRefundRequest& request,
+        const std::string& idempotencyKey,
+        RefundCallback&& callback
+    );
+
+    void queryRefund(
+        const std::string& refundNo,
+        RefundCallback&& callback
+    );
+
+    void syncRefundStatusFromWechat(
+        const std::string& refundNo,
+        std::function<void(const std::string& status)>&& callback
+    );
+
+private:
+    std::shared_ptr<WechatPayClient> wechatClient_;
+    std::shared_ptr<drogon::orm::DbClient> dbClient_;
+    std::shared_ptr<IdempotencyService> idempotencyService_;
+};
